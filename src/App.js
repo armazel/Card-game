ca52a30026b5204b58c22f9f0bfa16cdf0097c6c
afonua94 <dragun.egor@gmail.com>
2018-04-06 14:49:55 +0300

    add flow

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './containers/Home'
 import Counter from './containers/Counter'
 import AboutUs from './containers/AboutUs'
 import {BlurRouteButton} from './components/styled/buttons'
-import {AppContainer,AppWrapper} from "./components/styled/appBlock";
+import {AppContainer} from "./components/styled/appBlock";
 import 'material-design-icons';
 import {PrivateRouteAboutUs,PrivateRouteCounter,} from './protected_routes/routes'
 
@@ -13,11 +13,11 @@ import {PrivateRouteAboutUs,PrivateRouteCounter,} from './protected_routes/route
 class App extends Component {
   render() {
     return (
-        <AppWrapper>
+        <div>
             <AppContainer>
-                <BlurRouteButton iconType="home" classType="material-icons" to='/' description='Главная' fontSize='54px' padding="80px"></BlurRouteButton>
-                <BlurRouteButton iconType='theaters' classType="material-icons" to='/aboutUs' description='Об игре' fontSize='54px' padding="80px"></BlurRouteButton>
-                <BlurRouteButton iconType='add' classType="material-icons" to='/counter' description='Правила игры' fontSize='54px' padding="80px"></BlurRouteButton>
+                <BlurRouteButton iconType="home" classType="material-icons" to='/' description='Home' fontSize='54px' padding="80px"></BlurRouteButton>
+                <BlurRouteButton iconType='track_changes' classType="material-icons" to='/aboutUs' description='About Us' fontSize='54px' padding="80px"></BlurRouteButton>
+                <BlurRouteButton iconType='add' classType="material-icons" to='/counter' description='Counter' fontSize='54px' padding="80px"></BlurRouteButton>
             </AppContainer>
 
             <main>
@@ -27,7 +27,7 @@ class App extends Component {
                     <PrivateRouteCounter  path='/counter' component={Counter}/>
                 </Switch>
             </main>
-        </AppWrapper>
+        </div>
     );
   }
 }
